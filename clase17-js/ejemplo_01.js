128d48ac670b6b787321b1cb2fac47ee4f519e26
¨Clara <carboniclara012ing@gmail.com>
2024-06-09 23:01:35 -0300

    clase 17

---
+++ clase17-js/ejemplo_01.js
@@ -0,0 +1,15 @@
+const numeros = [1,23,45,65,23,25];
+
+const dobles = numeros.map(function (num) {
+    return num*2;
+})
+
+const triples = numeros.map(function (num) {
+    return num*3;
+})
+
+console.log(numeros);
+
+console.log(dobles);
+
+console.log(triples);
